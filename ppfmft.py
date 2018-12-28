41e90b6865ef7da9631b2d6f1f3d1eb2abe8bb24
Aziza Kalmasova <38079432+aziza-calm@users.noreply.github.com>
2018-12-28 19:35:34 +0300

    Update ppfmft.py

--- ppfmft.py
+++ ppfmft.py
@@ -2,11 +2,11 @@ def __init_plugin__(app):
     app.menuBar.addmenuitem('Plugin', 'command',
         label='Pymol future Dock Plugin',
         command=lambda: mytkdialog(app.root))
-	
+
 def leftmouse():
 	import os
 	os.system("python /home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/fmft_dock.py /home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/1avx_l_nmin.pdb /home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/1avx_r_nmin.pdb /home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/fmft_weights_ei.txt")
-	
+
 def mytkdialog(parent):
 	import Tkinter as tk
 	import ttk
