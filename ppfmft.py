daaaaf4a96a2181ef036e3d7656c4e6ae67d7a57
Aziza Kalmasova <38079432+aziza-calm@users.noreply.github.com>
2019-01-08 21:34:09 +0300

    Copy und Entry
    Added: 
    *copying of selected receptor and ligand
    *entry for specifying the path to the /fmft_code_dev folder

--- ppfmft.py
+++ ppfmft.py
@@ -1,19 +1,26 @@
+import Tkinter as tk
+from pymol import cmd
+
 def __init_plugin__(app):
     app.menuBar.addmenuitem('Plugin', 'command',
-        label='Pymol future Dock Plugin',
+        label='Dock Plugin',
         command=lambda: mytkdialog(app.root))
 
-#Action for button Dock! (the only button we have)
+#Action for button Start
 #runs fmft_dock.py
-def leftmouse(rec, lig):
+def run_dock(dirname, recname, ligname):
 	import subprocess
-	import Tkinter
 	import tkMessageBox
 	import time
-	srcfmft = "/home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/fmft_dock.py"
-	#lig = "/home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/1avx_l_nmin.pdb"
-	#rec = "/home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/1avx_r_nmin.pdb"
-	wei = "/home/aziza/Downloads/basa/fmft_code_dev/install-local/bin/fmft_weights_ei.txt"
+	import shutil
+	import tempfile
+	tmpdir = tempfile.mkdtemp(dir = dirname)
+	rec = tmpdir + "/receptor.pdb"
+	cmd.save(rec, recname)
+	lig = tmpdir + "/ligand.pdb"
+	cmd.save(lig, ligname)
+	srcfmft = dirname + "/install-local/bin/fmft_dock.py"
+	wei = dirname + "/install-local/bin/fmft_weights_ei.txt"
 	fmftcmd = ['python', srcfmft, lig, rec, wei] 
 	p = subprocess.Popen(fmftcmd, stderr=subprocess.PIPE)
 	while p.returncode is None:
@@ -28,9 +35,21 @@ def leftmouse(rec, lig):
 		tkMessageBox.showinfo("Information","Done! :)")
 	else:
 		tkMessageBox.showerror("Error","Something went wrong :(")
+	shutil.rmtree(tmpdir)
+
+def fmftpath(x, y):
+	pathw = tk.Tk()
+	pathw.title("Path")
+	fmftpath_label = tk.Label(pathw, text="Specify the path to the /fmft_code_dev folder first")
+	fmftpath_label.grid(row=2, column=0)
+	fmftpath_entry = tk.Entry(pathw, width=50)
+	fmftpath_entry.grid(row=3, column=0)
+	fmftpath_entry.insert(0, "/home/aziza/Downloads/basa/fmft_code_dev")
+	fmftpath_entry.bind('<Return>', run_dock)
+	buttonDock=tk.Button(pathw,text='Start',width=6,height=1,bg='blue',fg='white',font='verdana 14', command = lambda: run_dock(fmftpath_entry.get(), x, y))
+	buttonDock.grid(column=1,row=4)
 	
 def mytkdialog(parent):
-	import Tkinter as tk
 	#we need ttk for comboboxes
 	import ttk
 	
@@ -38,15 +57,17 @@ def mytkdialog(parent):
 	root.geometry("500x200+100+80")
 	root.title("Pymol future Dock Plugin")
 	
-	receptors = ["receptor1", "receptor2"]
+	receptors = cmd.get_names(selection='(all)')
 	combobox1 = ttk.Combobox(root,values = receptors, height=3, state = 'readonly')
 	combobox1.set(u"Receptor")
 	combobox1.grid(column=0,row=0)
+	rec = receptors[combobox1.current()]
 	
- 	ligands = ["ligand1", "ligand2"]
+ 	ligands = cmd.get_names(selection='(all)')
 	combobox2 = ttk.Combobox(root, values = ligands, height=3, state = 'readonly')
 	combobox2.set(u"Ligand")
 	combobox2.grid(column=1,row=0)
+	lig = ligands[combobox2.current()]
 	
- 	buttonDock=tk.Button(root,text='Dock!',width=6,height=1,bg='blue',fg='white',font='arial 14', command = lambda: leftmouse(receptors[combobox1.current()], ligands[combobox2.current()]))
+ 	buttonDock=tk.Button(root,text='Dock!',width=6,height=1,bg='blue',fg='white',font='arial 14', command = lambda:  fmftpath(rec, lig))
  	buttonDock.grid(column=2,row=1)
