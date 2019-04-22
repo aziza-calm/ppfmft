eed191450de25c14f455ebcd61907589cd18e299
Aziza Kalmasova <aziza.kalmasova@phystech.edu>
2019-04-22 11:38:44 +0300

    less cast and tmp_free in memory function

--- ppfmft.py
+++ ppfmft.py
@@ -29,15 +29,15 @@ def memory():
     """
     with open('/proc/meminfo', 'r') as mem:
         ret = {}
-        tmp = 0
+        tmp_free = 0
         for i in mem:
             sline = i.split()
-            if str(sline[0]) == 'MemTotal:':
-                ret['total'] = int(sline[1])
-            elif str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
-                tmp += int(sline[1])
-        ret['free'] = tmp
-        ret['used'] = int(ret['total']) - int(ret['free'])
+            if sline[0] == 'MemTotal:':
+                ret['total'] = sline[1]
+            elif sline[0] in ('MemFree:', 'Buffers:', 'Cached:'):
+                tmp_free += int(sline[1])
+        ret['free'] = tmp_free
+        ret['used'] = int(ret['total']) - ret['free']
     return ret
 
 
