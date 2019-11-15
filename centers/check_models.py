bf3c46f6b09700dbf42ac65632dc4aba6f5aa58e
Aziza Kalmasova <aziza.kalmasova@phystech.edu>
2019-11-15 17:59:51 +0300

    check if models inside the sphere script added

--- centers/check_models.py
+++ centers/check_models.py
@@ -2,12 +2,13 @@ from Bio.PDB import *
 import glob
 import json
 
-DIAM = 28.0 # diameter of the spheres
-path = "/home/aziza/Downloads/basa/pymol/ppfmft/centers/mol_test/1oph_1/"
+DIAM = 90.0 # diameter of the spheres
+path = "/home/aziza/Downloads/basa/pymol/ppfmft/centers/mol_test/1gxd_2/"
 
 with open('/home/aziza/Downloads/basa/pymol/ppfmft/centers/centers.json') as json_file:
     data = json.load(json_file)
-center = data[0]
+center = data[1]
+print("Center: {}".format(center))
 
 ligs = glob.glob(path + "*.pdb")
 ligs_in = []
@@ -19,7 +20,7 @@ for lig in ligs:
         x, y, z = atom.get_coord()
         if (x - center[0])**2 + (y - center[1])**2 + (z - center[2])**2 < (DIAM / 2)**2:
             ligs_in.append(lig)
-            continue
-
+            break
+print("{} out of {} is inside the sphere".format(len(ligs_in), len(ligs)))
 with open('ligs_in.json', 'w') as outfile:
     json.dump(ligs_in, outfile)
