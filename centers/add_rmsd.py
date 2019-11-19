edd5f98dc810d6cc7ba2c8d5c7322070ba37806a
Aziza Kalmasova <aziza.kalmasova@phystech.edu>
2019-11-19 15:47:57 +0300

    added a new key 'rmsd' to ligs_sorted and write it in another json sorted_with_rmsd.json

---
+++ centers/add_rmsd.py
@@ -0,0 +1,28 @@
+# import sblu.rmsd
+import numpy as np
+import prody
+import json
+
+model = '/home/aziza/Downloads/basa/pymol/ppfmft/centers/mol_test/1gxd_l.pdb'
+
+with open('ligs_sorted.json') as json_file:
+    ligs = json.load(json_file)
+
+def rmsd(x, y):
+    delta = x - y
+    N = len(delta)
+    np.multiply(delta, delta, delta)
+    return np.sqrt((delta.sum() / N))
+
+crys = prody.proteins.parsePDB(model)
+for i in range(len(ligs)):
+    lig = prody.proteins.parsePDB(ligs[i]['path'])
+    crys_coords = crys.getCoords()
+    lig_coords = lig.getCoords()
+    ligs[i]['rmsd'] = rmsd(crys_coords, lig_coords)
+
+for i in range(len(ligs)):
+    print(ligs[i])
+
+with open("sorted_with_rmsd.json", "w") as output_file:
+    json.dump(ligs, output_file)
