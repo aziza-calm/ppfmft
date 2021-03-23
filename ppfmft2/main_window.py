5d438bebd5c847c779e79d659ca04df406506b22
aziza <aziza.kalmasova@phystech.edu>
2021-03-24 02:42:46 +0300

    runs fmft :)

--- ppfmft2/main_window.py
+++ ppfmft2/main_window.py
@@ -1,9 +1,17 @@
 import os
+import tempfile
+import pymol
+import subprocess
 from pymol.Qt.utils import loadUi
-from pymol.Qt import QtGui, QtCore, QtWidgets
-from pymol.Qt.utils import (getSaveFileNameWithExt, UpdateLock, WidgetMenu,
-                            PopupOnException,
-                            connectFontContextMenu, getMonospaceFont)
+from pymol.Qt import QtWidgets
+
+
+def set_object_list(combobox):
+    objects = pymol.cmd.get_names('objects')
+    combobox.clear()
+    combobox.addItems(objects)
+    combobox.setCurrentIndex(len(objects) - 1)
+    return combobox
 
 
 class MainWindow(QtWidgets.QDialog):
@@ -11,13 +19,33 @@ class MainWindow(QtWidgets.QDialog):
         super(MainWindow, self).__init__()
         uifile = os.path.join(os.path.dirname(__file__), 'dock_window.ui')
         form = loadUi(uifile, self)
-        self.ligand_combobox = self.set_object_list(self.ligand_combobox)
-        self.receptor_combobox = self.set_object_list(self.receptor_combobox)
+        self.ligand_combobox = set_object_list(self.ligand_combobox)
+        self.receptor_combobox = set_object_list(self.receptor_combobox)
+        self.button_dock.clicked.connect(self.dock_pressed)
 
-    def set_object_list(self, combobox):
-        import pymol
-        objects = pymol.cmd.get_names('objects')
-        combobox.clear()
-        combobox.addItems(objects)
-        combobox.setCurrentIndex(len(objects) - 1)
-        return combobox
+    def dock_pressed(self):
+        print("Dock button pressed")
+        recname = self.receptor_combobox.currentText()
+        ligname = self.ligand_combobox.currentText()
+        # Creating a temporary directory
+        tmpdir = tempfile.mkdtemp()
+
+        # Making copies of receptor and ligand into tmpdir
+        rec = tmpdir + "/receptor.pdb"
+        pymol.cmd.save(rec, recname)
+        lig = tmpdir + "/ligand.pdb"
+        pymol.cmd.save(lig, ligname)
+
+        # Preparations for running fmft (creating a string command for Popen)
+        dirname = "/home/aziza/Documents/sciense/fmft_suite"
+        srcfmft = dirname + "/install-local/bin/fmft_dock.py"
+        wei = dirname + "/install-local/bin/prms/fmft_weights_ei.txt"
+        NRES = 1000
+        fmftcmd = ["python", srcfmft, "--nres", str(NRES), rec, lig, wei]
+        print(fmftcmd)
+        # Run!
+        p = subprocess.Popen(fmftcmd, cwd=tmpdir)
+        rc = p.returncode
+        if rc is not None:
+            print("The process seems to finish")
+            print("Maybe you can find something in " + tmpdir)
