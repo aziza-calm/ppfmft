a0a6ac573f87ac89654703dbf563d3c1e4bbb3cc
aziza <aziza.kalmasova@phystech.edu>
2021-04-25 11:57:41 +0300

    Docking logs are catched! QtProcess instead subprocess

--- ppfmft2/main_window.py
+++ ppfmft2/main_window.py
@@ -1,10 +1,7 @@
 import os
 import tempfile
-import pymol
-import subprocess
-from pymol.Qt.utils import loadUi
-from qt_wrapper import QtWidgets, QtGui, WITH_PYMOL
-import sblu
+
+from qt_wrapper import QtWidgets, QtCore, WITH_PYMOL, uic
 
 
 def fmft_path():
@@ -15,19 +12,57 @@ def fmft_path():
     return fmftpath
 
 
+def dialog(message):
+    qdialog = QtWidgets.QMessageBox()
+    qdialog.setIcon(QtWidgets.QMessageBox.Question)
+    qdialog.setWindowTitle("Warning")
+    qdialog.setText(message)
+    qdialog.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    return qdialog.exec_() == QtWidgets.QMessageBox.Ok
+
+
 class MainWindow(QtWidgets.QDialog):
+    PDB_FILE_WIDGETS = ('pdbFileLabel', 'pdbFileSelector')
+    PYMOL_OBJECT_WIDGETS = ('pymolObjectLabel', 'pymolObjectCombo', 'refreshObjectsButton')
+
     def __init__(self, settings):
         super(MainWindow, self).__init__()
         uifile = os.path.join(os.path.dirname(__file__), 'main_window.ui')
-        loadUi(uifile, self)
-        self.ligand_combobox = self.set_object_list(self.ligand_combobox)
-        self.receptor_combobox = self.set_object_list(self.receptor_combobox)
+        uic.loadUi(uifile, self)
+        self.setup_objects_list()
+        self.setup_radio_buttons()
         self.settings = settings
         self.button_dock.clicked.connect(self.dock_pressed)
         self.settings_button.clicked.connect(self.settings_pressed)
+        self.p = None
+
+    def setup_objects_list(self):
+        if WITH_PYMOL:
+            import pymol
+            objects = pymol.cmd.get_names('objects')
+            self.ligand_combobox.clear()
+            self.ligand_combobox.addItems(objects)
+            self.ligand_combobox.setCurrentIndex(len(objects) - 1)
+            self.receptor_combobox.clear()
+            self.receptor_combobox.addItems(objects)
+            self.receptor_combobox.setCurrentIndex(len(objects) - 1)
+        else:
+            self.pymolObjectRadio.setEnabled(False)
+
+    def setup_radio_buttons(self):
+        if WITH_PYMOL:
+            self.pdbFileRadio.setChecked(False)
+            self.pymolObjectRadio.setChecked(True)
+        else:
+            self.pdbFileRadio.setChecked(True)
+            self.pymolObjectRadio.setChecked(False)
+
+    def message(self, s):
+        self.text.appendPlainText(s)
 
     def dock_pressed(self):
         # Check if translation matrices are precomputed
+        print(self.settings.fmftpath)
         if os.path.exists(self.settings.fmftpath + '/install-local/fmft_data/'):
             if not os.listdir(self.settings.fmftpath + '/install-local/fmft_data/'):
                 self.dialog("Couldn't find the appropriate translation matrix file")
@@ -40,46 +75,58 @@ class MainWindow(QtWidgets.QDialog):
         ligname = self.ligand_combobox.currentText()
         # Creating a temporary directory
         tmpdir = tempfile.mkdtemp()
-
+        print(tmpdir)
         # Making copies of receptor and ligand into tmpdir
-        rec = tmpdir + "/receptor.pdb"
-        pymol.cmd.save(rec, recname)
-        lig = tmpdir + "/ligand.pdb"
-        pymol.cmd.save(lig, ligname)
-
-        # Preparations for running fmft (creating a string command for Popen)
-        dirname = "/home/aziza/Documents/sciense/fmft_suite"
-        srcfmft = dirname + "/install-local/bin/fmft_dock.py"
-        wei = dirname + "/install-local/bin/prms/fmft_weights_ei.txt"
-        NRES = 1000
-        fmftcmd = ["python", srcfmft, "--nres", str(NRES), rec, lig, wei]
-        print(fmftcmd)
-        # Run!
-        p = subprocess.Popen(fmftcmd, cwd=tmpdir)
-        rc = p.returncode
-        if rc is not None:
-            print("The process seems to finish")
-            print("Maybe you can find something in " + tmpdir)
+        if WITH_PYMOL:
+            rec = tmpdir + "/receptor.pdb"
+            # pymol.cmd.save(rec, recname)
+            print("receptor saved in " + rec)
+            lig = tmpdir + "/ligand.pdb"
+            # pymol.cmd.save(lig, ligname)
+            print("ligand saved in " + lig)
         else:
-            print("FMFT failed")
+            rec = "/home/aziza/1avx_r_nmin.pdb"
+            lig = "/home/aziza/1avx_l_nmin.pdb"
+            print(lig)
+        if self.p is None:  # No process running.
+            print("Process is about to start\n")
+            self.message("Executing process")
+            self.p = QtCore.QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
+            self.p.readyReadStandardOutput.connect(self.handle_stdout)
+            self.p.readyReadStandardError.connect(self.handle_stderr)
+            self.p.stateChanged.connect(self.handle_state)
+            self.p.finished.connect(self.process_finished)  # Clean up once complete.
+            # Preparations for running fmft (creating a string command for Popen)
+            dirname = "/home/aziza/Documents/sciense/fmft_suite"
+            srcfmft = dirname + "/install-local/bin/fmft_dock.py"
+            wei = dirname + "/install-local/bin/prms/fmft_weights_ei.txt"
+            NRES = 1000
+            fmftcmd = [srcfmft, "--nres", str(NRES), rec, lig, wei]
+            print(fmftcmd)
+            self.p.start("python", fmftcmd)
+
+    def handle_stderr(self):
+        data = self.p.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        self.message(stderr)
+
+    def handle_stdout(self):
+        data = self.p.readAllStandardOutput()
+        stdout = bytes(data).decode("utf8")
+        self.message(stdout)
+
+    def handle_state(self, state):
+        states = {
+            QtCore.QProcess.NotRunning: 'Not running',
+            QtCore.QProcess.Starting: 'Starting',
+            QtCore.QProcess.Running: 'Running',
+        }
+        state_name = states[state]
+        self.message(f"State changed: {state_name}")
+
+    def process_finished(self):
+        self.message("Process finished.")
+        self.p = None
 
     def settings_pressed(self):
         self.settings.show()
-
-    @staticmethod
-    def set_object_list(combobox):
-        if WITH_PYMOL:
-            objects = pymol.cmd.get_names('objects')
-            combobox.clear()
-            combobox.addItems(objects)
-            combobox.setCurrentIndex(len(objects) - 1)
-        return combobox
-
-    @staticmethod
-    def dialog(message):
-        dialog = QtWidgets.QMessageBox()
-        dialog.setIcon(QtWidgets.QMessageBox.Question)
-        dialog.setWindowTitle("Warning")
-        dialog.setText(message)
-        dialog.setStandardButtons(QtWidgets.QMessageBox.Ok)
-        return dialog.exec_() == QtWidgets.QMessageBox.Ok
--- ppfmft2/qt_wrapper.py
+++ ppfmft2/qt_wrapper.py
@@ -1,11 +1,8 @@
-try:
-    from pymol.Qt import QtWidgets, QtCore, QtGui
-    WITH_PYMOL = True
-except ImportError:
-    from PyQt5 import QtWidgets, QtCore, QtGui
-    WITH_PYMOL = False
+# try:
+#     from pymol.Qt import QtWidgets, QtCore, QtGui
+#     WITH_PYMOL = True
+# except ImportError:
+from PyQt5 import QtWidgets, QtCore, QtGui
+WITH_PYMOL = False
 
-try:
-    from PyQt5 import uic
-except ImportError:
-    from PyQt4 import uic
+from PyQt5 import uic
